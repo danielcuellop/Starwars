5818f16711af6afbc9ebc95b6bfd9f4d7d8338cf
danielcuellop <franciscocuello86@gmail.com>
2023-09-27 16:24:26 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Dropdown2 from "./dropdown";
 
-
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-dark mb-3 container">
-			<Link to="/">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" alt="Bootstrap" width="100" height="70" />
-			</Link>
+    return (
+        <nav className="navbar navbar-light bg-dark mb-3 container">
+            <Link to="/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" alt="Bootstrap" width="100" height="70" />
+            </Link>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link active m-2" aria-current="page" href="#" style={{ fontSize: '24px', color: "yellow" }}>Planetas</a>
+                </li>
+            </ul>
 
-			<div className="ml-auto">
-				{/* <Link to="/demo">
-					<button className="btn bg-dark text-warning">Favorites</button>
-				</Link> */}
-				<Dropdown2 />
-			</div>
-		</nav>
-	);
+            <div className="ml-auto">
+                {/* <Link to="/demo">
+                    <button className="btn bg-dark text-warning">Favorites</button>
+                </Link> */}
+                <Dropdown2 />
+            </div>
+        </nav>
+    );
 };
